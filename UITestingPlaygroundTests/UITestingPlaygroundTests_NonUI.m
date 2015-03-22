1e7fb58cfaaca98e5d0f379c149ba2111a712a42
Ellen Shapiro <designatednerd@gmail.com>
2015-03-21 19:15:56 -0500

    Update non-UI tests to use XCTestExpectation

--- UITestingPlaygroundTests/UITestingPlaygroundTests_NonUI.m
+++ UITestingPlaygroundTests/UITestingPlaygroundTests_NonUI.m
@@ -6,11 +6,11 @@
 //  Copyright (c) 2014 Ellen Shapiro. All rights reserved.
 //
 
-#import "XCTAsyncTestCase.h"
-
 #import "VIFakeAPI.h"
 
-@interface UITestingPlaygroundTests_NonUI : XCTAsyncTestCase
+@import XCTest;
+
+@interface UITestingPlaygroundTests_NonUI : XCTestCase
 @property (nonatomic, strong) VIFakeAPI *fakeAPI;
 @end
 
@@ -34,48 +34,48 @@ NSTimeInterval const VIAPITimeout = 10;
 
 - (void)testFakeAPIValidLogin
 {
-    [self prepare];
+    XCTestExpectation *expectation = [self expectationWithDescription:@"Valid login succeeded"];
     [self.fakeAPI loginWithUsername:VIValidLoginUserName password:VIValidLoginPassword completion:^(BOOL success, NSError *error) {
         XCTAssertTrue(success, @"Valid login not returning success!");
         XCTAssertNil(error, @"Valid login returning non-nil error!");
-        [self notify:kXCTUnitWaitStatusSuccess];
+        [expectation fulfill];
     }];
     
-    [self waitForStatus:kXCTUnitWaitStatusSuccess timeout:VIAPITimeout];
+    [self waitForExpectationsWithTimeout:VIAPITimeout handler:nil];
 }
 
 
 - (void)testFakeAPIInvalidUserName
 {
-    [self prepare];
+    XCTestExpectation *expectation = [self expectationWithDescription:@"Invalid Username Failed"];
     [self.fakeAPI loginWithUsername:@"fake@fake.com" password:VIValidLoginPassword completion:^(BOOL success, NSError *error) {
         XCTAssertFalse(success, @"Invalid login returning success!");
         XCTAssertNotNil(error, @"Invalid login not returning error!");
         NSString *localizedDescription = error.userInfo[NSLocalizedDescriptionKey];
         XCTAssertTrue([localizedDescription isEqualToString:[VIFakeAPI invalidUsernameError]], @"Invalid password returning incorrect error!");
-        [self notify:kXCTUnitWaitStatusSuccess];
+        [expectation fulfill];
     }];
     
-    [self waitForStatus:kXCTUnitWaitStatusSuccess timeout:VIAPITimeout];
+    [self waitForExpectationsWithTimeout:VIAPITimeout handler:nil];
 }
 
 - (void)testFakeAPIInvalidPassword
 {
-    [self prepare];
+    XCTestExpectation *expectation = [self expectationWithDescription:@"Invalid password failed"];
     [self.fakeAPI loginWithUsername:VIValidLoginUserName password:@"password" completion:^(BOOL success, NSError *error) {
         XCTAssertFalse(success, @"Invalid login returning success!");
         XCTAssertNotNil(error, @"Invalid login not returning error!");
         NSString *localizedDescription = error.userInfo[NSLocalizedDescriptionKey];
         XCTAssertTrue([localizedDescription isEqualToString:[VIFakeAPI invalidPasswordError]], @"Invalid password  returning incorrect error!");
-        [self notify:kXCTUnitWaitStatusSuccess];
+        [expectation fulfill];
     }];
     
-    [self waitForStatus:kXCTUnitWaitStatusSuccess timeout:VIAPITimeout];
+    [self waitForExpectationsWithTimeout:VIAPITimeout handler:nil];
 }
 
 - (void)testFakeAPIInvalidUserNameAndPassword
 {
-    [self prepare];
+    XCTestExpectation *expectation = [self expectationWithDescription:@"Invalid both failed"];
     [self.fakeAPI loginWithUsername:@"nope@nope.com" password:@"noooooope" completion:^(BOOL success, NSError *error) {
         XCTAssertFalse(success, @"Invalid login returning success");
         XCTAssertNotNil(error, @"Invalid login not returning error!");
@@ -87,16 +87,16 @@ NSTimeInterval const VIAPITimeout = 10;
         NSRange passwordErrorRange = [localizedDescription rangeOfString:[VIFakeAPI invalidPasswordError]];
         XCTAssertNotEqual(passwordErrorRange.location, NSNotFound, @"Error does not contain invalid password info!");
         
-        [self notify:kXCTUnitWaitStatusSuccess];
+        [expectation fulfill];
     }];
     
-    [self waitForStatus:kXCTUnitWaitStatusSuccess timeout:VIAPITimeout];
+    [self waitForExpectationsWithTimeout:VIAPITimeout handler:nil];
 }
 
 
 - (void)testFakeAPINetworkTimeout
 {
-    [self prepare];
+    XCTestExpectation *expectation = [self expectationWithDescription:@"Fake network timed out"];
     [self.fakeAPI networkFailureLoginWithUsername:VIValidLoginUserName password:VIValidLoginPassword completion:^(BOOL success, NSError *error) {
         XCTAssertFalse(success, @"Fake network failure not failing!");
         XCTAssertNotNil(error, @"Fake network failure not returning an error!");
@@ -104,10 +104,10 @@ NSTimeInterval const VIAPITimeout = 10;
         NSString *localizedDescription = error.userInfo[NSLocalizedDescriptionKey];
         XCTAssertTrue([localizedDescription isEqualToString:[VIFakeAPI networkFailError]], @"Error does not contain network fail info!");
         
-        [self notify:kXCTUnitWaitStatusSuccess];
+        [expectation fulfill];
     }];
     
-    [self waitForStatus:kXCTUnitWaitStatusSuccess timeout:VIAPITimeout];
+    [self waitForExpectationsWithTimeout:VIAPITimeout handler:nil];
 }
 
 @end
